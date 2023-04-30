cf0d80fd38505344dc81c6a06d8ce6f4ab4987c5
Andrew Gilliland <andrew.gilliland@csm.cc>
2023-04-30 18:22:19 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andrew Gilliland on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
